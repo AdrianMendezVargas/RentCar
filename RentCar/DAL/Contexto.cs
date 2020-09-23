90d89685bf835063d89b36b13d91ecaa4bffcd78
adrian mendez <eladri-@live.com>
2020-09-22 20:49:25 -0400

    Add project files.

---
+++ RentCar/DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentCar.DAL {
+    public class Contexto : DbContext{
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+
+            optionsBuilder.UseSqlite("Data source= RentCar.db");
+        }
+
+    }
+}
