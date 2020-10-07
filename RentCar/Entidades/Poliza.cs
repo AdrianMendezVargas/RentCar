0d85745eec8a6c4644a2da67f72ef10681e2d4d8
Adrian Alcides Mendez Vargas <adrian_mendez@ucne.edu.do>
2020-10-06 21:16:25 -0400

    Modificando reporte de polizas

---
+++ RentCar/Entidades/Poliza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RentCar.Entidades {
+    public class Poliza {
+
+        [Key]
+        public int PolizaId { get; set; }
+        public int VehiculoId { get; set; }
+        public decimal MontoAsegurado { get; set; }
+        public DateTime FechaInicial { get; set; }
+        public DateTime FechaFinal { get; set; }
+        public DateTime FechaLimitePago { get; set; }
+        public virtual string FechaFinalCorta => FechaFinal.ToShortDateString();
+    }
+}
--- RentCar/UI/Reportes/ReportePolizas.xaml.cs
+++ RentCar/UI/Reportes/ReportePolizas.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using RentCar.Entidades;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,21 +19,49 @@ namespace RentCar.UI.Reportes {
     /// Interaction logic for ReportePolizas.xaml
     /// </summary>
     public partial class ReportePolizas : Window {
+        
+
+        public string FechaActual { get; set; } = DateTime.Now.ToShortDateString();
+        public List<Poliza> Polizas { get; set; } = new List<Poliza>();
+
+        const int DIAS_ANTICIPACION = 30;
+
         public ReportePolizas() {
             InitializeComponent();
+            this.DataContext = this;
+
+            BuscarPolizasARenovar();
+           
+        }
+
+        private void BuscarPolizasARenovar() {
+
+            Polizas.Add(new Poliza { PolizaId = 1 , VehiculoId = 1 , FechaFinal = new DateTime(2020 , 11 , 5) });
+            Polizas.Add(new Poliza { PolizaId = 1 , VehiculoId = 1 , FechaFinal = new DateTime(2020 , 11 , 5) });
+            Polizas.Add(new Poliza { PolizaId = 1 , VehiculoId = 1 , FechaFinal = new DateTime(2020 , 11 , 5) });
+            Polizas.Add(new Poliza { PolizaId = 1 , VehiculoId = 1 , FechaFinal = new DateTime(2020 , 11 , 5) });
+            Polizas.Add(new Poliza { PolizaId = 1 , VehiculoId = 1 , FechaFinal = new DateTime(2020 , 11 , 5) });
+
+
+            Polizas = Polizas.Where(p => p.FechaFinal.DayOfYear - DIAS_ANTICIPACION <= DateTime.Today.DayOfYear).ToList();
+            
+            PolizasListBox.ItemsSource = null;
+            PolizasListBox.ItemsSource = Polizas;
+
         }
 
         private void ImprimirButton_Click(object sender , RoutedEventArgs e) {
             try {
-                this.IsEnabled = false;
+                this.ImprimirButton.IsEnabled = false;
                 PrintDialog printDialog = new PrintDialog();
                 if (printDialog.ShowDialog() == true) {
                     printDialog.PrintVisual(print , "Reporte");
                 }
             } finally {
-                this.IsEnabled = true;
+                this.ImprimirButton.IsEnabled = true;
                 MessageBox.Show("Finalizado","Exito",MessageBoxButton.OK,MessageBoxImage.Information);
             }
         }
+
     }
 }
