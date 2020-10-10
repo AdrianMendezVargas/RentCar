ce7bd02ed3d1eb2e2ec04fc7e40fdd92bf560202
NiurbisReyesJaquez <71194774+NiurbisReyesJaquez@users.noreply.github.com>
2020-10-10 11:43:51 -0400

    Entidad Vehiculo

---
+++ RentCar/Entidades/Vehiculo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RentCar.Entidades
+{
+    public class Vehiculo
+    {
+        [Key]
+        public int VehiculoId { get; set; }
+        public int PolizaId { get; set; }
+        public string Matricula { get; set; }
+        public string Placa { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public DateTime AñoFabricacion { get; set; }
+        public string Estado { get; set; }
+        public string Precio { get; set; }
+        public string Chassis { get; set; }
+        public string Pasajeros { get; set; }
+        public string Puertas { get; set; }
+        public string Traccion { get; set; }
+        public string Comentario { get; set; }
+        public string Valor { get; set; }
+        public string Tipo { get; set; }
+
+        public Vehiculo()
+        {
+            VehiculoId = 0;
+            PolizaId = 0;
+            Matricula = "";
+            Marca = "";
+            Modelo = "";
+            Placa = "";
+            AñoFabricacion = DateTime.Now;
+            Estado = "";
+            Precio = "";
+            Chassis = "";
+            Pasajeros = "";
+            Puertas = "";
+            Traccion = "";
+            Comentario = "";
+            Valor = "";
+            Tipo = "";
+
+
+        }
+
+    }
+}
